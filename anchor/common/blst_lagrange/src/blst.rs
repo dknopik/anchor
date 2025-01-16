8b6ee9509a2ec6a380190df9a4da7ba2f1aaba7f
Daniel Knopik <daniel@dknopik.de>
2025-01-16 10:17:51 +0100

    improve single thread performance

--- anchor/common/blst_lagrange/src/blst.rs
+++ anchor/common/blst_lagrange/src/blst.rs
@@ -7,6 +7,7 @@ use rand::prelude::*;
 use std::iter::{once, repeat_with};
 use std::mem::MaybeUninit;
 use std::num::NonZeroU64;
+use std::ptr;
 use zeroize::Zeroizing;
 
 #[derive(Debug, Clone)]
@@ -106,7 +107,6 @@ pub fn split_with_rng(
         .collect()
 }
 
-#[cfg(not(feature = "blst_single_thread"))]
 pub fn combine_signatures(signatures: &[Signature], ids: &[KeyId]) -> Result<Signature, Error> {
     if signatures.len() < 2 {
         return Err(Error::LessThanTwoSignatures);
@@ -159,86 +159,34 @@ pub fn combine_signatures(signatures: &[Signature], ids: &[KeyId]) -> Result<Sig
         }
     }
 
-    Ok(Signature::from_point(
-        signatures.mult(&d, 255).to_signature(),
-        false,
-    ))
+    Ok(Signature::from_point(mult(&signatures, &d), false))
 }
 
-// variant not using blsts pooling, seems to be slower. might still make sense for us because we
-// want to control threading
+#[cfg(not(feature = "blst_single_thread"))]
+fn mult(signatures: &[min_pk::Signature], d: &[u8]) -> min_pk::Signature {
+    signatures.mult(d, 255).to_signature()
+}
+
 #[cfg(feature = "blst_single_thread")]
-pub fn combine_signatures(signatures: &[Signature], ids: &[KeyId]) -> Result<Signature, Error> {
-    if signatures.len() < 2 {
-        return Err(Error::LessThanTwoSignatures);
-    }
-    if signatures.len() != ids.len() {
-        return Err(Error::NotOneIdPerSignature);
-    }
-
-    let signatures = signatures
-        .iter()
-        .map(|sig| sig.point().cloned().ok_or(Error::InvalidSignature))
-        .collect::<Result<Vec<_>, _>>()?;
-
-    // intermediates.
-    let mut ifr = MaybeUninit::<blst_fr>::uninit();
-    let mut is = MaybeUninit::<blst_scalar>::uninit();
-    let mut ip2 = MaybeUninit::<blst_p2>::uninit();
-    let mut ip2a = MaybeUninit::<blst_p2_affine>::uninit();
-
-    let zero = unsafe {
-        blst_fr_from_uint64(ifr.as_mut_ptr(), &0);
-        ifr.assume_init()
-    };
-
-    let mut numerator = ids[0].clone().fr;
+fn mult(signatures: &[min_pk::Signature], d: &[u8]) -> min_pk::Signature {
+    let mut ret = blst_p2::default();
+    let mut ret_affine = blst_p2_affine::default();
+    let p: [*const blst_p2_affine; 2] = [<&blst_p2_affine>::from(&signatures[0]), ptr::null()];
+    let s: [*const u8; 2] = [&d[0], ptr::null()];
     unsafe {
-        for id in &ids[1..] {
-            blst_fr_mul(&mut numerator, &numerator, &id.fr);
-        }
+        let mut scratch: Vec<u64> =
+            Vec::with_capacity(blst_p2s_mult_pippenger_scratch_sizeof(signatures.len()) / 8);
+        blst_p2s_mult_pippenger(
+            &mut ret,
+            &p[0],
+            signatures.len(),
+            &s[0],
+            255,
+            scratch.as_mut_ptr(),
+        );
+        blst_p2_to_affine(&mut ret_affine, &ret)
     }
-    if numerator == zero {
-        return Err(Error::ZeroId);
-    }
-
-    let mut first = true;
-    let mut result = MaybeUninit::<blst_p2>::uninit();
-    unsafe {
-        for (id_i, sig_i) in ids.iter().zip(&signatures) {
-            let mut denominator = id_i.fr.clone();
-            for id_j in ids.iter() {
-                if id_i as *const KeyId != id_j as *const KeyId {
-                    blst_fr_sub(ifr.as_mut_ptr(), &id_j.fr, &id_i.fr);
-                    if ifr.assume_init_ref() == &zero {
-                        return Err(Error::RepeatedId);
-                    }
-                    blst_fr_mul(&mut denominator, &denominator, ifr.as_ptr());
-                }
-            }
-            blst_fr_inverse(&mut denominator, &denominator);
-            blst_fr_mul(ifr.as_mut_ptr(), &denominator, &numerator);
-            blst_scalar_from_fr(is.as_mut_ptr(), ifr.as_ptr());
-            blst_p2_from_affine(ip2.as_mut_ptr(), <&blst_p2_affine>::from(sig_i));
-            blst_p2_mult(
-                ip2.as_mut_ptr(),
-                ip2.as_ptr(),
-                &is.assume_init_ref().b as *const u8,
-                255,
-            );
-            if first {
-                result.write(ip2.assume_init());
-                first = false;
-            } else {
-                blst_p2_add(result.as_mut_ptr(), result.as_ptr(), ip2.as_ptr());
-            }
-        }
-    }
-
-    unsafe {
-        blst_p2_to_affine(ip2a.as_mut_ptr(), result.as_ptr());
-        Ok(Signature::from_point(ip2a.assume_init().into(), false))
-    }
+    ret_affine.into()
 }
 
 pub(crate) fn random_key(rng: &mut (impl CryptoRng + Rng)) -> Result<bls::SecretKey, Error> {
